d7b7619c6b9c311ed54d7a6e099ee2a6d3bcfcc0
Olivier Doucet <olivier@oxeva.fr>
2012-02-14 18:54:08 +0100

    add things in README improve error handling in iostat follow Zend  coding standard

--- iostat.php
+++ iostat.php
@@ -1,10 +1,9 @@
 <?php
-
-/************************
+/**
  * IOSTAT Extended
  * @author Olivier Doucet <olivier@oxeva.fr>
  * @version 0.01
- ************************/
+ **/
 
 // @todo argv
 
@@ -15,18 +14,23 @@ if (isset($argv[1])) {
 }
 
 // Variables needed
-$arrayDM    = array(); // DM name to ID mapping
-$arrayDRBD  = array(); // DRBD name to DM id mapping
-$arrayDRBDid= array(); // DRBD name to DRBD id
+$arrayDM        = array(); // DM name to ID mapping
+$arrayDRBD      = array(); // DRBD name to DM id mapping
+$arrayDRBDid    = array(); // DRBD name to DRBD id
 
-$arrayStats = array(); // /sys/block/xx/stat content
+$arrayStats     = array(); // /sys/block/xx/stat content
 $arrayStatsDiff = array(); // copy of arrayStats for differentials
 
 echo "Preparing data ...\n";
 
 // Create mapping DM-X to LVM
-$lvsOutput = explode("\n", shell_exec('lvs -o vg_name,lv_name,devices,lv_kernel_minor --noheadings --separator :'));
-
+$lvsOutput = explode(
+    "\n", 
+    shell_exec(
+        'lvs -o vg_name,lv_name,devices,lv_kernel_minor '.
+        '--noheadings --separator :'
+    )
+);
 foreach ($lvsOutput as $row) {
     $infos = explode(':', trim($row));
     if ($infos[0] != '' && $infos[1] != '') {
@@ -34,6 +38,8 @@ foreach ($lvsOutput as $row) {
         $arrayDRBD[substr($infos[2], 0, strpos($infos[2], '('))][] = (int) $infos[3];
     }
 }
+if (count($arrayDM) == 0) die("ERROR: lvs not present or no lvs in this system\n");
+
 unset($lvsOutput, $infos, $row);
 
 ksort($arrayDRBD);
@@ -69,7 +75,11 @@ do {
     // Print part
     printf("%s\n%45s : riops   wiops      rK/s       wK/s \n", str_repeat('=', 60), 'vg/lv');
     foreach ($arrayDRBD as $drbdName => $drbdIds) {
-        show_stats($drbdName, $arrayStats['drbd'.$arrayDRBDid[$drbdName]], $arrayStatsDiff['drbd'.$arrayDRBDid[$drbdName]]);
+        show_stats(
+            $drbdName, 
+            $arrayStats['drbd'.$arrayDRBDid[$drbdName]], 
+            $arrayStatsDiff['drbd'.$arrayDRBDid[$drbdName]]
+        );
         
         foreach ($arrayDM as $dmName => $dmId) {
             if (in_array($dmId, $drbdIds))
@@ -79,15 +89,23 @@ do {
     
     $arrayStatsDiff = $arrayStats;
     usleep(SLEEP*1000000);
-} while(true);
+} while (true);
 
-function show_stats($dmName, $arrayStats, $arrayStatsDiff) {
+/** 
+ * Print pretty output
+ * @var $dmName  device name
+ * @var $arrayStats latest values
+ * @var $arrayStats old values
+ */
+function show_stats($dmName, $arrayStats, $arrayStatsDiff)
+{
     $diffRIOPS = $arrayStats[0] - $arrayStatsDiff[0];
     $diffWIOPS = $arrayStats[4] - $arrayStatsDiff[4];
     $diffRK    = $arrayStats[2] - $arrayStatsDiff[2];
     $diffWK    = $arrayStats[6] - $arrayStatsDiff[6];
     
-    printf("%-45s %6d   %6d    %6d    %6d\n", 
+    printf(
+        "%-45s %6d   %6d    %6d    %6d\n", 
         $dmName, 
         ($diffRIOPS == 0) ? '-' : $diffRIOPS/SLEEP,
         ($diffWIOPS == 0) ? '-' : $diffWIOPS/SLEEP,
